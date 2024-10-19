c7d5a2388cdc2b7ecae4f7444109dc979b15ed65
Radhika Vyas <radhikavyas@radhikas-mbp.mynetworksettings.com>
2024-10-19 02:35:40 -0700

    Added ./model

--- test_inference.py
+++ test_inference.py
@@ -5,7 +5,7 @@ from model.face_recognition import face_match  # Importing the face_match functi
 test_image = 'test_000.jpg'
 
 # Perform face matching
-result = face_match(test_image, 'model/data.pt')  # Using 'data.pt' for pre-saved embeddings
+result = face_match(test_image, './model/data.pt')  # Using 'data.pt' for pre-saved embeddings
 
 # Output the result
 print(f"Inference result: {result[0]}, Distance: {result[1]}")
